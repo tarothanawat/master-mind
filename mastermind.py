c94cbcdad3e24ec04083ff33b79741428c58889f
tarothanawat <115664618+tarothanawat@users.noreply.github.com>
2023-11-15 19:01:06 +0700

    The base game is completed at the minimum level.

--- mastermind.py
+++ mastermind.py
@@ -1,14 +1,14 @@
 import random
 
 def generate_four_digit_number():
-    # Generate a 4-digit number using digits 1-8
+    # Generate a 4-digit number using digits 1-6
     number = ""
     for _ in range(4):
-        digit = random.randint(1, 6)  # Generate a random digit between 1 and 8
+        digit = random.randint(1, 6)  # Generate a random digit between 1 and 6
         number += str(digit)  # Append the digit to the number string
     return number
 
-# Generate and print a 4-digit number using digits 1-8
+# Generate and print a 4-digit number using digits 1-6
 
 class MasterMind:
     def __init__(self):
@@ -17,12 +17,20 @@ class MasterMind:
     def check_if_correct(self, guess):
         display = ""
         count = 0
+        colors = "123456"
+        correct = ""
+        if guess == 'help':
+            print(self.generated_num)
+            return "cheat"
         if len(guess) != 4:
             return "invalid"
         for i in range(len(guess)):
-            if guess[i] == self.generated_num[i]:
+            if guess[i] not in colors:
+                return "invalid color"
+            elif guess[i] == self.generated_num[i]:
                 display += "*"
                 count += 1
+                correct += str(i)
             elif guess[i] in self.generated_num:
                 display += "o"
         return display
@@ -37,6 +45,13 @@ class MasterMind:
             ans = self.check_if_correct(guess)
             if ans == "invalid":
                 print("Invalid guess! Your guess must contain 4 digits.")
+                print()
+                continue
+            if ans == "invalid color":
+                print("Invalid color code! Color code must be within digit 1-6.")
+                print()
+                continue
+            if ans == 'cheat':
                 print()
                 continue
             print(ans)
